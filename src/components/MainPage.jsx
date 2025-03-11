b133dae17c9fb23411695c006c4c494c3b3e2fe5
akashps <itzmeakashps@gmail.com>
2025-03-11 18:14:09 +0530

    Version 2.9.1 added

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -29,7 +29,7 @@ const MainPage = () => {
 
       {/* Version Number */}
       <div className="fixed top-5 right-5 bg-white text-gray-800 px-4 py-2 rounded-full shadow-md z-10">
-        v.2.9
+        v.2.9.1
       </div>
 
 

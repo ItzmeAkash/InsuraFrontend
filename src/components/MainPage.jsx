b658e1ed3e5b5b4e80d8e20f0afff03aa19ac970
ItzmeAkash <itzmeakashps@gmail.com>
2025-12-08 20:23:37 +0530

    v.2.10

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -28,7 +28,7 @@ const MainPage = () => {
 
       {/* Version Number */}
       <div className="fixed top-5 right-5 bg-white text-gray-800 px-4 py-2 rounded-full shadow-md z-10">
-        v.2.9.2.7
+        v.2.10
       </div>
 
 
